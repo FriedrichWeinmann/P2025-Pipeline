b5868f589458f8c2ec9d7b2d071f9055104ee381
FriedrichWeinmann <FriedrichWeinmann@users.noreply.github.com>
2025-03-26 20:32:49 +0100

    update

--- B-02-AppsInAction.ps1
+++ B-02-AppsInAction.ps1
@@ -3,3 +3,7 @@
 # https://github.com/FriedrichWeinmann/EntraAuth.Graph.Application
 Install-Module EntraAuth.Graph.Application -Scope CurrentUser
 Connect-EntraService -ClientID Graph -Scopes 'Application.ReadWrite.All', 'AppRoleAssignment.ReadWrite.All'
+
+<#
+Demo was skipped due to time constraints, sorry
+#>
---
+++ readme.md
@@ -0,0 +1,8 @@
+﻿# PDQ Wednesday Lunch & Learn
+
+Presented on 2025-03-26
+
+Topics:
+
++ Pipeline
++ [EntraAuth.Graph.Application](https://github.com/FriedrichWeinmann/EntraAuth.Graph.Application)
